88e464344a855697c218667b4d2f8af76be327b7
Josh Faust <josh@torch3d.com>
2017-09-08 11:41:57 -0700

    Support multiple query definitions per query file, allowing them to be imported by name

--- loader.js
+++ loader.js
@@ -39,14 +39,86 @@ function expandImports(source, doc) {
   return outputCode;
 }
 
+// Collect any fragment/type references from a node, adding them to the refs Set
+function collectFragmentReferences(node, refs) {
+  if (node.kind === "FragmentSpread") {
+    refs.add(node.name.value);
+  } else if (node.kind === "VariableDefinition") {
+    const type = node.type;
+    if (type.kind === "NamedType") {
+      refs.add(type.name.value);
+    }
+  }
+
+  if (node.selectionSet) {
+    for (const selection of node.selectionSet.selections) {
+      collectFragmentReferences(selection, refs);
+    }
+  }
+
+  if (node.variableDefinitions) {
+    for (const def of node.variableDefinitions) {
+      collectFragmentReferences(def, refs);
+    }
+  }
+
+  if (node.definitions) {
+    for (const def of node.definitions) {
+      collectFragmentReferences(def, refs);
+    }
+  }
+}
+
 module.exports = function(source) {
   this.cacheable();
   const doc = gql`${source}`;
-  const outputCode = `
+  let outputCode = `
+    function oneQuery(doc, refs, operationName) {
+      const newDoc = Object.assign({}, doc);
+      // Filter out operations
+      newDoc.definitions = newDoc.definitions.filter(function(def) {
+        return def.kind !== "OperationDefinition" || def.name.value === operationName;
+      });
+      
+      // Now, for the operation we're running, only include fragments it references
+      const refsSet = new Set(refs);
+      newDoc.definitions = newDoc.definitions.filter(function(def) {
+        return def.kind !== "FragmentDefinition" || refsSet.has(def.name.value);
+      });
+    
+      return newDoc;
+    }
+
     var doc = ${JSON.stringify(doc)};
     doc.loc.source = ${JSON.stringify(doc.loc.source)};
+    module.exports = {};
   `;
-  const importOutputCode = expandImports(source, doc);
 
-  return outputCode + os.EOL + importOutputCode + os.EOL + `module.exports = doc;`;
+  // Allow multiple query/mutation definitions in a file. This parses out dependencies
+  // at compile time, and then uses those at load time to create minimal query documents
+  // We cannot do the latter at compile time due to how the #import code works.
+  let operationCount = 0;
+  for (const op of doc.definitions) {
+    if (op.kind === "OperationDefinition") {
+      ++operationCount;
+      const opName = op.name.value;
+      const opQueryRefs = new Set();
+      collectFragmentReferences(op, opQueryRefs);
+      outputCode += `
+      var ${opName}Refs = ${JSON.stringify(Array.from(opQueryRefs))};
+      module.exports["${opName}"] = oneQuery(doc, ${opName}Refs, "${opName}");
+      `
+    }
+  }
+
+  if (operationCount <= 1) {
+    outputCode += `
+      module.exports = doc;
+    `
+  }
+
+  const importOutputCode = expandImports(source, doc);
+  const allCode = outputCode + os.EOL + importOutputCode + os.EOL;
+
+  return allCode;
 };
