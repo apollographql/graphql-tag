ac4f0da2a3bc07f33312e5af9707a5fb2034edcd
Josh Faust <jfaust@users.noreply.github.com>
2017-12-06 10:05:10 -0800

    Webpack: support multiple query definitions per query file (#122)
    * Support multiple query definitions per query file, allowing them to be imported by name
    
    * Fix reference tracking for references between fragments, and add loader tests for the multi-query case
    
    * Allow #import to work with files that have multiple queries/mutations in them.
    This is not really possible to test in the curren test framework, because it requires the load-time require()
    
    * Generate code with var instead of const, since we can't assume an ES6 consumer
    
    * for .. of is also ES6
    
    * let -> var

--- loader.js
+++ loader.js
@@ -42,11 +42,133 @@ function expandImports(source, doc) {
 module.exports = function(source) {
   this.cacheable();
   const doc = gql`${source}`;
-  const outputCode = `
+  let headerCode = `
     var doc = ${JSON.stringify(doc)};
     doc.loc.source = ${JSON.stringify(doc.loc.source)};
   `;
-  const importOutputCode = expandImports(source, doc);
 
-  return outputCode + os.EOL + importOutputCode + os.EOL + `module.exports = doc;`;
+  let outputCode = "";
+
+  // Allow multiple query/mutation definitions in a file. This parses out dependencies
+  // at compile time, and then uses those at load time to create minimal query documents
+  // We cannot do the latter at compile time due to how the #import code works.
+  let operationCount = doc.definitions.reduce(function(accum, op) {
+    if (op.kind === "OperationDefinition") {
+      return accum + 1;
+    }
+
+    return accum;
+  }, 0);
+
+  if (operationCount <= 1) {
+    outputCode += `
+      module.exports = doc;
+    `
+  } else {
+    outputCode +=`
+    // Collect any fragment/type references from a node, adding them to the refs Set
+    function collectFragmentReferences(node, refs) {
+      if (node.kind === "FragmentSpread") {
+        refs.add(node.name.value);
+      } else if (node.kind === "VariableDefinition") {
+        var type = node.type;
+        if (type.kind === "NamedType") {
+          refs.add(type.name.value);
+        }
+      }
+
+      if (node.selectionSet) {
+        node.selectionSet.selections.forEach(function(selection) {
+          collectFragmentReferences(selection, refs);
+        });
+      }
+
+      if (node.variableDefinitions) {
+        node.variableDefinitions.forEach(function(def) {
+          collectFragmentReferences(def, refs);
+        });
+      }
+
+      if (node.definitions) {
+        node.definitions.forEach(function(def) {
+          collectFragmentReferences(def, refs);
+        });
+      }
+    }
+
+    var definitionRefs = {};
+    (function extractReferences() {
+      doc.definitions.forEach(function(def) {
+        if (def.name) {
+          var refs = new Set();
+          collectFragmentReferences(def, refs);
+          definitionRefs[def.name.value] = refs;
+        }
+      });
+    })();
+
+    function findOperation(doc, name) {
+      return doc.definitions.find(function(op) {
+        return op.name ? op.name.value == name : false;
+      });
+    }
+    
+    function oneQuery(doc, operationName) {
+      // Copy the DocumentNode, but clear out the definitions
+      var newDoc = Object.assign({}, doc);
+
+      var op = findOperation(doc, operationName);
+      newDoc.definitions = [op];
+      
+      // Now, for the operation we're running, find any fragments referenced by
+      // it or the fragments it references
+      var opRefs = definitionRefs[operationName] || new Set();
+      var allRefs = new Set();
+      var newRefs = new Set(opRefs);
+      while (newRefs.size > 0) {
+        var prevRefs = newRefs;
+        newRefs = new Set();
+
+        prevRefs.forEach(function(refName) {
+          if (!allRefs.has(refName)) {
+            allRefs.add(refName);
+            var childRefs = definitionRefs[refName] || new Set();
+            childRefs.forEach(function(childRef) {
+              newRefs.add(childRef);
+            });
+          }
+        });
+      }
+
+      allRefs.forEach(function(refName) {
+        var op = findOperation(doc, refName);
+        if (op) {
+          newDoc.definitions.push(op);
+        }
+      });
+      
+      return newDoc;
+    }
+
+    module.exports = doc;
+    `
+
+    for (const op of doc.definitions) {
+      if (op.kind === "OperationDefinition") {
+        if (!op.name) {
+          throw "Query/mutation names are required for a document with multiple definitions";
+        }
+
+        const opName = op.name.value;
+        outputCode += `
+        module.exports["${opName}"] = oneQuery(doc, "${opName}");
+        `
+      }
+    }
+  }
+
+  const importOutputCode = expandImports(source, doc);
+  const allCode = headerCode + os.EOL + importOutputCode + os.EOL + outputCode + os.EOL;
+
+  return allCode;
 };
