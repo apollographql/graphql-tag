cf75a00cde0b1bccd780c09316c2a8d8bf9c1cad
Jon Wong <j@jnwng.com>
2018-02-19 12:40:11 -0800

    Adding compatibility check for all supported versions of `graphql` (#156)
    * Adding compatibility testing for all `graphql` versions
    
    * Running `tav` only in CI and latest otherwise
    
    * Bumping to capture more versions of `graphql`
    
    * Only check compatibility
    
    * Try running `yarn test` instead
    
    * Run `tav` _after_ testing latest

--- test/graphql.js
+++ test/graphql.js
@@ -1,6 +1,6 @@
-const gqlRequire = require('./src');
-const gqlDefault = require('./src').default;
-const loader = require('./loader');
+const gqlRequire = require('../src');
+const gqlDefault = require('../src').default;
+const loader = require('../loader');
 const assert = require('chai').assert;
 
 [gqlRequire, gqlDefault].forEach((gql, i) => {
@@ -227,169 +227,16 @@ const assert = require('chai').assert;
       assert.isTrue(gql`{ sameQuery }` === gql`  { sameQuery,   }`);
     });
 
-    it('is correct for a simple query', () => {
-      const ast = gql`
-        {
-          user(id: 5) {
-            firstName
-            lastName
-          }
-        }
-      `;
-
-      assert.equal(ast.kind, "Document");
-      assert.deepEqual(ast.definitions, [
-        {
-          "kind": "OperationDefinition",
-          "operation": "query",
-          "name": null,
-          "variableDefinitions": null,
-          "directives": [],
-          "selectionSet": {
-            "kind": "SelectionSet",
-            "selections": [
-              {
-                "kind": "Field",
-                "alias": null,
-                "name": {
-                  "kind": "Name",
-                  "value": "user"
-                },
-                "arguments": [
-                  {
-                    "kind": "Argument",
-                    "name": {
-                      "kind": "Name",
-                      "value": "id"
-                    },
-                    "value": {
-                      "kind": "IntValue",
-                      "value": "5"
-                    }
-                  }
-                ],
-                "directives": [],
-                "selectionSet": {
-                  "kind": "SelectionSet",
-                  "selections": [
-                    {
-                      "kind": "Field",
-                      "alias": null,
-                      "name": {
-                        "kind": "Name",
-                        "value": "firstName"
-                      },
-                      "arguments": [],
-                      "directives": [],
-                      "selectionSet": null
-                    },
-                    {
-                      "kind": "Field",
-                      "alias": null,
-                      "name": {
-                        "kind": "Name",
-                        "value": "lastName"
-                      },
-                      "arguments": [],
-                      "directives": [],
-                      "selectionSet": null
-                    }
-                  ]
-                }
-              }
-            ]
-          }
-        }
-      ]);
-    });
+    const fragmentAst = gql`
+    fragment UserFragment on User {
+      firstName
+      lastName
+    }
+  `;
 
     it('returns the same object for the same fragment', () => {
       assert.isTrue(gql`fragment same on Same { sameQuery }` ===
         gql`fragment same on Same { sameQuery }`);
-    });
-
-    it('is correct for a fragment', () => {
-      const ast = gql`
-        fragment UserFragment on User {
-          firstName
-          lastName
-        }
-      `;
-
-      assert.equal(ast.kind, "Document");
-      assert.deepEqual(ast.definitions, [
-        {
-          "kind": "FragmentDefinition",
-          "name": {
-            "kind": "Name",
-            "value": "UserFragment"
-          },
-          "typeCondition": {
-            kind: "NamedType",
-            "name": {
-              "kind": "Name",
-              "value": "User"
-            }
-          },
-          "directives": [],
-          "selectionSet": {
-            "kind": "SelectionSet",
-            "selections": [
-              {
-                "kind": "Field",
-                "alias": null,
-                "name": {
-                  "kind": "Name",
-                  "value": "firstName"
-                },
-                "arguments": [],
-                "directives": [],
-                "selectionSet": null
-              },
-              {
-                "kind": "Field",
-                "alias": null,
-                "name": {
-                  "kind": "Name",
-                  "value": "lastName"
-                },
-                "arguments": [],
-                "directives": [],
-                "selectionSet": null
-              }
-            ]
-          }
-        }
-      ]);
-    });
-
-    const fragmentAst = gql`
-      fragment UserFragment on User {
-        firstName
-        lastName
-      }
-    `;
-    it('can reference a fragment passed as a document', () => {
-      const ast = gql`
-        {
-          user(id: 5) {
-            ...UserFragment
-          }
-        }
-        ${fragmentAst}
-      `;
-
-      assert.deepEqual(ast, gql`
-        {
-          user(id: 5) {
-            ...UserFragment
-          }
-        }
-        fragment UserFragment on User {
-          firstName
-          lastName
-        }
-      `);
     });
 
     it('returns the same object for the same document with substitution', () => {
