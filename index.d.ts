7c413afc230ec42bec8793ffcb15ff596b58ea2e
Yosuke Kurami <yosuke.kurami@gmail.com>
2019-01-08 01:46:39 +0900

    chore: add type for experimentalFragmentVariables

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
 export default function gql(literals: any, ...placeholders: any[]): any;
 export function resetCaches(): void;
 export function disableFragmentWarnings(): void;
+export function enableExperimentalFragmentVariables(): void;
+export function disableExperimentalFragmentVariables(): void;
